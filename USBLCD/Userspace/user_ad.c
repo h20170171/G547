1d693547f75cbb2b4a9905c04f819a9422bf7434
h20170171 <35378103+h20170171@users.noreply.github.com>
2018-04-27 15:11:53 +0530

    Update user_ad.c

--- USBLCD/Userspace/user_ad.c
+++ USBLCD/Userspace/user_ad.c
@@ -9,77 +9,86 @@
 #include <usb.h>
 #define USBDEV_SHARED_VENDOR    0x16C0  
 #define USBDEV_SHARED_PRODUCT   0x05DC 
+#define USB_ERROR_NOTFOUND  1
+#define USB_ERROR_ACCESS    2
+#define USB_ERROR_IO        3
 usbGetStringAscii(usb_dev_handle *dev, int index, int langid, char *buf, int buflen)
 {
 	char    buffer[256];
 	int     rval, i;
 	/*Get the descriptor from the usb device*/
-    if((rval = usb_control_msg(dev, USB_ENDPOINT_IN, USB_REQ_GET_DESCRIPTOR, (USB_DT_STRING << 8) + index, langid, buffer, sizeof(buffer), 1000)) < 0)
-        return rval;
-    if(buffer[1] != USB_DT_STRING)
-        return 0;
-    if((unsigned char)buffer[0] < rval)
-        rval = (unsigned char)buffer[0];
-    rval /= 2;
+    	if((rval = usb_control_msg(dev, USB_ENDPOINT_IN, USB_REQ_GET_DESCRIPTOR, (USB_DT_STRING << 8) + index, langid, buffer, sizeof(buffer), 1000)) < 0)
+        	return rval;
+    	if(buffer[1] != USB_DT_STRING)
+        	return 0;
+    	if((unsigned char)buffer[0] < rval)
+        	rval = (unsigned char)buffer[0];
+    	rval /= 2;
     
-   for(i=1;i<rval;i++){
-        if(i > buflen) 
-            break;
-        buf[i-1] = buffer[2 * i];
-        if(buffer[2 * i + 1] != 0) 
+   	for(i=1;i<rval;i++)
+	{
+        	if(i > buflen) 
+            	break;
+        	buf[i-1] = buffer[2 * i];
+        	if(buffer[2 * i + 1] != 0) 
             buf[i-1] = '?';
-    }
-    buf[i-1] = 0;
-    return i-1;
+    	}
+    	buf[i-1] = 0;
+    	return i-1;
 }
-#define USB_ERROR_NOTFOUND  1
-#define USB_ERROR_ACCESS    2
-#define USB_ERROR_IO        3
+
 static int usbOpenDevice(usb_dev_handle **device, int vendor, char *vendorName, int product, char *productName)
 {
-/*Pointer to usb_bus structure*/
-struct usb_bus      *bus;
-/*Pointer to usb_device structure*/
-struct usb_device   *dev;
-usb_dev_handle      *handle = NULL;
-int                 errorCode = USB_ERROR_NOTFOUND;
-static int          didUsbInit = 0;
+	/*Pointer to usb_bus structure*/
+	struct usb_bus      *bus;
+	/*Pointer to usb_device structure*/
+	struct usb_device   *dev;
+	usb_dev_handle      *handle = NULL;
+	int                 errorCode = USB_ERROR_NOTFOUND;
+	static int          didUsbInit = 0;
 
-    if(!didUsbInit){
-        didUsbInit = 1;
-        usb_init();
-    }
-    usb_find_busses();
-    usb_find_devices();
-    for(bus=usb_get_busses(); bus; bus=bus->next){
-        for(dev=bus->devices; dev; dev=dev->next){
-            if(dev->descriptor.idVendor == vendor && dev->descriptor.idProduct == product){
-                char    string[256];
-                int     len;
-                handle = usb_open(dev); 
-                if(!handle){
-                    errorCode = USB_ERROR_ACCESS;
-		     fprintf(stderr, "Warning: cannot open USB device: %s\n", usb_strerror());
-                    continue;
-                }
-                if(vendorName == NULL && productName == NULL){ 
-                    break;
+    	if(!didUsbInit)
+	{
+        	didUsbInit = 1;
+        	usb_init();
+        }
+    	usb_find_busses();
+    	usb_find_devices();
+    	for(bus=usb_get_busses(); bus; bus=bus->next)
+	{
+        	for(dev=bus->devices; dev; dev=dev->next){
+            	if(dev->descriptor.idVendor == vendor && dev->descriptor.idProduct == product)
+		{
+                	char    string[256];
+                	int     len;
+                	handle = usb_open(dev); 
+                	if(!handle)
+			{
+                    		errorCode = USB_ERROR_ACCESS;
+		     		fprintf(stderr, "Warning: cannot open USB device: %s\n", usb_strerror());
+                    		continue;
+                        }
+                	if(vendorName == NULL && productName == NULL){ 
+                                break;
                 }
                 
                 len = usbGetStringAscii(handle, dev->descriptor.iManufacturer,
-0x0409, string, sizeof(string));
-                if(len < 0){
+		0x0409, string, sizeof(string));
+                if(len < 0)
+		{
                     errorCode = USB_ERROR_IO;
                     fprintf(stderr, "Warning: cannot query manufacturer for device: %s\n", usb_strerror());
-                }else{
+                }
+		else{
                     errorCode = USB_ERROR_NOTFOUND;
                     fprintf(stderr, "seen device from vendor ->%s<-\n", string);
                     if(strcmp(string, vendorName) == 0){
                         len = usbGetStringAscii(handle, dev->descriptor.iProduct, 0x0409, string, sizeof(string));
                         if(len < 0){
                             errorCode = USB_ERROR_IO;
-fprintf(stderr, "Warning: cannot query product for device: %s\n", usb_strerror());
-                        }else{
+		    fprintf(stderr, "Warning: cannot query product for device: %s\n", usb_strerror());
+                        }
+	 	    else{
                             errorCode = USB_ERROR_NOTFOUND;
                             fprintf(stderr, "seen product ->%s<-\n", string); 
                             if(strcmp(string, productName) == 0)
@@ -96,7 +105,7 @@ fprintf(stderr, "Warning: cannot query product for device: %s\n", usb_strerror()
     }
     if(handle != NULL){
         errorCode = 0;
- *device = handle;
+   *device = handle;
     }
     return errorCode;
 }
@@ -132,16 +141,17 @@ int main()
 	/*Initialise usb device*/
 	usb_init();
 	/*Opens the usb device with the vendor name "www.obdev.at" and product name "usblcd"*/
-    	if(usbOpenDevice(&handle, USBDEV_SHARED_VENDOR, "www.obdev.at", USBDEV_SHARED_PRODUCT, "usblcd") != 0){
-        fprintf(stderr, "Could not find USB device \"USBasp\" with vid=0x%x pid=0x%x\n", USBDEV_SHARED_VENDOR, USBDEV_SHARED_PRODUCT);
-        exit(1);
+    	if(usbOpenDevice(&handle, USBDEV_SHARED_VENDOR, "www.obdev.at", USBDEV_SHARED_PRODUCT, "usblcd") != 0)
+	{
+        	fprintf(stderr, "Could not find USB device \"USBasp\" with vid=0x%x pid=0x%x\n", USBDEV_SHARED_VENDOR, USBDEV_SHARED_PRODUCT);
+        	exit(1);
   	}
    	/*Opens the device*/
    	file=open(device,0);
    	if(file<0)
    	{
-      	printf("File did not open from user space\n");
-      	exit(-1);
+      		printf("File did not open from user space\n");
+      		exit(-1);
    	}
    	printf("Enter the command or data\n");
    	scanf("%[^\n]%*c",msg);
